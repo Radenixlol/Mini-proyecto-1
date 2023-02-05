4f48fa26e92e8f67735a3e0b6b7ba94442565cd3
RoyMartin <ranseartin@gmail.com>
2023-02-05 04:47:55 -0400

    Nav-Bar-responsive (bonita y odiada)

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 
-document.getElementById("NAV").addEventListener("click", animatebars)
+const navSlide = () => {
+    let NAV1 = document.getElementById("parts_NAV1")
+    let NAV2 = document.getElementById("parts_NAV2")
+    let NAV3 = document.getElementById("parts_NAV3")
+    let menu = document.querySelector(".links")
+    let navlinks = document.querySelectorAll(".links li")
 
-let NAV1 = document.getElementById("parts_NAV1")
-let NAV2 = document.getElementById("parts_NAV2")
-let NAV3 = document.getElementById("parts_NAV3")
-
-function animatebars(){
-    NAV1.classList.toggle("activeparts_NAV1")
-    NAV2.classList.toggle("activeparts_NAV2")
-    NAV3.classList.toggle("activeparts_NAV3")
-
+    document.getElementById("NAV").addEventListener("click", () => {
+        NAV1.classList.toggle("activeparts_NAV1")
+        NAV2.classList.toggle("activeparts_NAV2")
+        NAV3.classList.toggle("activeparts_NAV3")
+        menu.classList.toggle("menuactive")
+        navlinks.forEach((link, index)=> {
+            if(link.style.animation){
+                link.style.animation = ""
+            } else{
+                link.style.animation = `navlinkfrade 500ms ease forwards ${(index / 7)+ 0.5 }s`
+            }
+        })
+    })
 }
+
+navSlide()
