b781d30ff43a77302f4dd9368781ef848f50cf64
RoyMartin <ranseartin@gmail.com>
2023-02-05 14:53:38 -0400

    Skill-dinamicas-siuuuuuuuuuuuuu

--- main.js
+++ main.js
@@ -1,38 +1,28 @@
 const data_skill = [
-    {skill:html, porcent:80},
-    {skill:css, porcent:60},
-    {skill:js, porcent:20},
-    {skill:python, porcent:100},
-    {skill:java, porcent:40},
+    {skill:"html", porcent:"80"},
+    {skill:"css", porcent:"60"},
+    {skill:"js", porcent:"20"},
+    {skill:"python", porcent:"100"},
+    {skill:"java", porcent:"40"},
 ]
-
-const div= document.querySelector("box-k")
-for (const i in data_skill) {
-    const html_skill = 
-    `
+const div= document.querySelector(".box-k")
+const htmlskill = () => {
+data_skill.map(function(data_skill){
+    let html_skill = `
         <div class="skill">
             <div class="detalles">
-              <span>${data_skill[i].skill}</span>
-              <span>${data_skill[i].porcent}%</span>
+              <span>${data_skill.skill}</span>
+              <span>${data_skill.porcent}%</span>
             </div>
             <div class="barra">
-              <div class="barra-${data_skill[i].porcent}"></div>
+              <div class="barra-${data_skill.porcent}"></div>
             </div>
         </div>
     `
     div.innerHTML += html_skill;
-        
-    
+           
+})
+ 
 }
-    const html_skill = 
-    `
-        <div class="skill">
-            <div class="detalles">
-              <span>{</span>
-              <span></span>
-            </div>
-            <div class="barra">
-              <div class="barra-20"></div>
-            </div>
-        </div>
-    `
+
+htmlskill()
